4df621373a8b66a10ef7f3f86fac929bf488f558
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2021-01-29 10:44:46 +0530

    array related programs are updated today

---
+++ Array/doubleArray.java
@@ -0,0 +1,25 @@
+// to show the working of 1-D arrays
+import java.util.Scanner;
+
+class doubleArray{
+ 
+    public static void main(String args[]){
+
+        double [] arr; // declaring an array but no memory allocated yet
+        int n; // size of the array
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the number of elements : ");
+        
+        n = obj.nextInt();
+        arr = new int[n]; // allocating the memory to the array
+
+        System.out.println("Enter the value of "+n+" elements :");
+
+        for(int i = 0;i<arr.length;i++)
+        arr[i] = obj.nextDouble();
+
+        System.out.println("\nThe elements of array are : \n");
+        for(int i=0;i<arr.length;i++)
+        System.out.println("--> "+arr[i]);
+    }
+}
---
+++ Array/initialization.java
@@ -0,0 +1,17 @@
+// to show the working of 1-D arrays
+import java.util.Scanner;
+
+class initialization{
+ 
+    public static void main(String args[]){
+
+       // int arr[] = new int[10];
+       // int [] arr = new int[5];
+       int [] arr = {1,2,3,4,5};
+       // int [] arr = new int[] {1,2,3,4,5};
+
+       // here arr.length gives us the length of the array or size of the array;
+       for(int i=0;i<arr.length; i++)
+       System.out.print(arr[i]+" ");
+    }
+}
