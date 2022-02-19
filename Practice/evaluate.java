1ad7a49d93e4cfaead80c2faac81f2da21e86ee8
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2022-02-19 16:04:27 +0530

    push

--- Practice/evaluate.java
+++ Practice/evaluate.java
@@ -1,14 +1,8 @@
 public class evaluate{
-
+    static boolean ok;
     public static void main(String[] args){
-        String name1 = "Ganga";
-        String name2 = "Amazon";
-        
-        System.out.println(name1 instanceof String);
-        // System.out.println(name2 instanceof name1);
-        System.out.println(name1 instanceof Object);
-        System.out.println(args instanceof Object);
-        // System.out.println(0 = 2+=2*4);  
+        int STATIC = 4;
+        System.out.println(ok);
         
     }
 }
