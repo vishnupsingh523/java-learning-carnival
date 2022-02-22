057bb1c0b01ec592b14f660c36f210b1679fb776
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2022-02-22 09:20:31 +0530

    files added

---
+++ Mainf.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+public class Mainf{
+public static void main(String[] args) {
+
+    // int Info = 11; // Line 1
+    // String S = Integer . toString(Info); //Line 2
+    // String Temp = "139"; // Line 3
+    // int Data = Integer . parseInt (Temp); //Line 4
+    // System.out.printIn(Data + S); //Line 5
+}
+}
---
+++ practice.java
@@ -0,0 +1,21 @@
+
+
+public class practice{
+    public static void main(String[] args){
+        int var = 57, anotherVar = 21, result;
+        String str = "85";
+        String anotherStr = "49";
+        result = var*anotherStr/anotherVar;
+        if(result<7)
+        System.out.println(str);
+        else
+        System.out.println(anotherStr);
+
+        }
+
+        static int d0Calc(int a){
+            a = a*9;
+            return a;
+        }
+    }
+
